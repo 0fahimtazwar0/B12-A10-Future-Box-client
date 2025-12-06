bc4674111bafd2e9377a8723be6a4a7dfdfaa20f
0fahimtazwar0 <0fahimtazwar0@gmail.com>
2025-12-06 20:09:03 +0600

    added Footer

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Home from "./pages/Home";
+import Footer from "./components/Footer";
 
 const Root = () => {
   return (
     <div>
       <Home />
+      <Footer />
     </div>
   );
 };
